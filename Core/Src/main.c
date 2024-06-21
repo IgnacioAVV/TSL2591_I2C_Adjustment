4b24353718c179a012071402b05f65d959019848
MYSTERIUS\ignac <ignacio.mysterius@gmail.com>
2024-06-21 16:22:09 -0400

    I2C2_INIT

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -111,6 +111,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
+  MX_I2C2_Init();
   MX_USART2_UART_Init();
   MX_USART3_UART_Init();
   MX_I2C2_Init();
